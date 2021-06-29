de41e99f68de845a477cf04591fba5aa1cf09826
sherchansachin <sachinsherchan3@gmail.com>
2021-06-30 00:00:05 +0545

    models created

--- sms/settings.py
+++ sms/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#since we are using the custom User model,
+AUTH_USER_MODEL = 'smsapp.User'
+
 
 # Application definition
 
--- smsapp/models.py
+++ smsapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default = False)
+    is_admin = models.BooleanField(default= False)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+
+class Admin(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    admin_email = models.EmailField(max_length=254)
+    objects = models.Manager()
+
+
+class Student(models.Model):
+
+    #one to one relationship with User  
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    # student has their personal email id, phone number, address and parent's name
+    
+    std_email = models.EmailField(max_length=254)
+    std_gender = models.CharField(max_length=50)
+    std_phone = models.CharField(max_length=20)
+    std_address = models.CharField(max_length=200)
+    parents_name = models.CharField(max_length=200)
+    objects = models.Manager()
+
+
+class Teacher(models.Model):
+
+    #one to one relationship with User  
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    # Teachers have their personal email id, phone number, address 
+   
+    t_email = models.EmailField(max_length=254)
+    t_phone = models.CharField(max_length=20)
+    t_address = models.CharField(max_length=200)
+    objects = models.Manager()
+
+    
