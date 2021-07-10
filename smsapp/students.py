cf7230c397cfac88f52a6e8245ec3283ffdb2c77
Gr8Sushant <spokharel95@gmail.com>
2021-07-10 18:14:34 +0545

    students.py created

---
+++ smsapp/students.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+
+from ..forms import StudentSignUpForm
+from ..models import User
+
+class StudentSignUpView(CreateView):
+    model = User
+    form_class = StudentSignUpForm
+    template_name = 'smsapp/std_register.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('students')
