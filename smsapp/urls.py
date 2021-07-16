ddc6f9e9349ff7609d980b442916e0f5f54be7f8
Gr8Sushant <spokharel95@gmail.com>
2021-07-16 15:21:07 +0545

    urls and views modified

--- smsapp/urls.py
+++ smsapp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import student_admission
 
 urlpatterns = [
-    path('', views.loginPage, name="login"),
-    path('register/', views.register, name='register'),
+    # path('', views.loginPage, name="login"),
+    path('', views.register, name='register'),
     path('studentregistration/', views.StudentRegister.as_view(), name='std_register'),
     path('teacherregistration/', views.TeacherRegister.as_view(), name='teach_register'),
+    path('admission_form/', student_admission, name='student_admission'),
 ]   
--- smsapp/views.py
+++ smsapp/views.py
@@ -1,24 +1,69 @@
-from .forms import StudentSignUp, TeacherSignUp
+# from .forms import StudentSignUp, TeacherSignUp
 from .models import User
 from django.shortcuts import render
 from django.views.generic import CreateView
 
-# Create your views here.
-
-def loginPage(request):
-    return render(request, 'smsapp/login.html')
-    
+from .forms import StudentSignUpForm,TeacherSignUpForm, Student_admission_form
+from .models import User,Student
 
 def register(request):
-    return render(request, 'smsapp/register.html')
+	return render(request,'smsapp/register.html')
 
 
 class StudentRegister(CreateView):
     model = User
-    form_class = StudentSignUp
+    form_class = StudentSignUpForm
     template_name = 'smsapp/std_register.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return render('smsapp/std_register.html')
+
+
+def student_admission(request):
+    form = Student_admission_form(request.POST, request.FILES)
+    if form.is_valid():
+        form = form.save(commit=False)
+        form.save()
+        return redirect('student_admission')
+
+    context = {'form': form}
+    return render(request, 'smsapp/std_admission.html', context)
+
 
 class TeacherRegister(CreateView):
     model = User
-    form_class = TeacherSignUp
+    form_class = TeacherSignUpForm
     template_name = 'smsapp/teach_register.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        #login(self.request, user)
+        return redirect('main')
+
+# def loginPage(request):
+#     return render(request, 'smsapp/login.html')
+    
+
+# def register(request):
+#     return render(request, 'smsapp/register.html')
+
+
+# class StudentRegister(CreateView):
+#     model = User
+#     # form_class = StudentSignUp
+#     template_name = 'smsapp/std_register.html'
+
+# class TeacherRegister(CreateView):
+#     model = User
+#     # form_class = TeacherSignUp
+#     template_name = 'smsapp/teach_register.html'
